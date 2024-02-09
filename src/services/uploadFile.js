17b891618c525d2279d20b2ad23fe518a71df938
Andrew Cornforth <andrew.cornforth@shift.online>
2024-02-09 13:36:57 +0000

    re-add success and fail folders

--- src/services/uploadFile.js
+++ src/services/uploadFile.js
@@ -9,10 +9,30 @@ const { connect, getClient } = require("./sftp");
 const { updateStatusBar } = require("./statusBar");
 
 const pendingFilesDirName = 'ftp_files_pending';
+const successUploadsDirName = 'ftp_files_uploaded_successfully'
+const errorUploadsDirName = 'ftp_files_failed';
 const maxFileSizeMb = 10;
 const acceptedFileTypes = []; // allow all
 const acceptedMimeTypes = ['text/plain', 'text/csv'];
 let validationError = null;
+
+async function createFolder(folderName, basePath) {
+    const folderPath = path.join(path.dirname(basePath), path.basename(folderName));
+    try {
+        await fs.access(folderPath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                await fs.mkdir(folderPath, { recursive: true });
+                console.log(`Created ${folderName} folder at ${folderPath}`);
+            } catch (mkdirError) {
+                logError(mkdirError, `Error creating ${folderName} folder: ${mkdirError.message}`);
+            }
+        } else {
+            logError(error, `Error accessing ${folderName} folder: ${error.message}`);
+        }
+    }
+};
 
 const uploadFiles = async (mainWindow, basePath, [...filePaths]) => {
 
@@ -24,6 +44,12 @@ const uploadFiles = async (mainWindow, basePath, [...filePaths]) => {
         // Validate the file before proceeding
         if (!validateUploadFile(filePath)) {
             updateStatusBar({ mainWindow, text: `Invalid file: ${fileName}. Reason: ${validationError}`, type: 'danger' });
+            try {
+                 moveFileToErrorFolder(basePath, filePath)
+            }
+            catch (err) {
+                console.log('moveFilesToErrorFolder (1) error', err)
+            }
             return null; // Skip to the next file
         }
 
@@ -57,10 +83,13 @@ const uploadFilesFTP = async (mainWindow, filePaths, basePath) => {
             const lastIndex = filePath.lastIndexOf(path.sep);
             const fileNameEnding = filePath.substring(lastIndex + 1);
             try {
-                await getClient().fastPut(filePath, targetPath);
-                updateStatusBar({ mainWindow, text: `File Uploaded: ${fileNameEnding}`, type: 'success' });
+                await getClient().fastPut(filePath, targetPath).then(async ()=> {
+                    updateStatusBar({ mainWindow, text: `File Uploaded: ${fileNameEnding}`, type: 'success' });
+                    await moveFileToSuccessUploadsFolder(basePath, filePath);
+                });
             } catch (error) {
                 logError(error, `Error uploading file "${fileName}" to FTP server: ${error.message}`);
+                await moveFileToErrorFolder(basePath, filePath);
                 throw error;
             }
         })
@@ -107,6 +136,51 @@ const validateUploadFile = (filePath) => {
     }
 
     return true;
+};
+
+/**
+ * Moves a file to the success uploads folder.
+ * @param basePath {string} The base path where the file is located.
+ * @param filePath {string} The path to the file.
+ * @returns {Promise<void>}
+ */
+const moveFileToSuccessUploadsFolder = async (basePath, filePath) => {
+    const successUploadsDirPath = path.join(path.dirname(basePath), successUploadsDirName);
+
+    try {
+        // Create the success folder if it doesn't exist
+        await createFolder(successUploadsDirPath, basePath)
+
+        // Construct the destination path
+        const destinationPath = path.join(successUploadsDirPath, path.basename(filePath));
+
+        // Move the file to the success uploads folder
+        await fs.rename(filePath, destinationPath);
+        console.log('File moved to success uploads folder:', destinationPath);
+    } catch (error) {
+        logError(error, 'Error moving file to success uploads folder:'+ error)
+    }
+};
+
+const moveFileToErrorFolder = async (basePath, filePath) => {
+    const errorPath = path.join(path.dirname(basePath), errorUploadsDirName);
+
+    try {
+        // Create the pending folder if it doesn't exist
+        await createFolder(errorUploadsDirName, basePath)
+
+        // Extract the file name from the file path
+        const fileName = path.basename(filePath);
+
+        // Construct the new path for the file in the pending_uploads folder
+        const newFilePath = path.join(errorPath, fileName);
+
+        // Move the file to the pending_uploads folder
+        await fs.rename(filePath, newFilePath);
+        console.log(`Moved file "${fileName}" to error folder`);
+    } catch (error) {
+        logError(error, `Error moving file to error folder: ${error.message}`)
+    }
 };
 
 module.exports = {
